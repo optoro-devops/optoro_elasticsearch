e0f67bbf323c0e19f44b345c78952c002087c732
Scott Medefind <smedefind@optoro.com>
2015-02-26 10:55:25 -0500

    Fixing cookbook dependencies and installing proper elasticsearch version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['java']['jdk_version'] = '7'
 default['java']['openjdk_package'] = 'openjdk-7-jre-headless'
 
+default['elasticsearch']['version'] = '1.4.4'
 default['elasticsearch']['jmx'] = 'true'
 default['elasticsearch']['cluster']['name'] = 'UNCONFIGURED'
--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'Installs ElasticSearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.0.3'
 
 depends 'apt'
 depends 'build-essential'
-depends 'aws', '~> 2.6.4'
+depends 'aws', '~> 2.5.3'
 depends 'monit', '~> 0.7.5'
 depends 'java', '~> 1.31.0'
 depends 'elasticsearch', '~> 0.3.13'
