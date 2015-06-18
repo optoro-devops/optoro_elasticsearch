32c3b3ba61982189c59cd3e4ad2a069000687ede
Scott Medefind <smedefind@optoro.com>
2015-06-18 13:34:54 -0400

    Upgrading to 1.6.0 ElasticSearch, removing old data disk

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,8 @@
 default['java']['jdk_version'] = '7'
 default['java']['openjdk_package'] = 'openjdk-7-jre-headless'
 
-default['elasticsearch']['version'] = '1.4.4'
+default['elasticsearch']['version'] = '1.6.0'
+default['elasticsearch']['host'] = 'https://download.elastic.co'
 default['elasticsearch']['jmx'] = 'true'
 default['elasticsearch']['cluster']['name'] = 'UNCONFIGURED'
 default['elasticsearch']['plugins'] = {
