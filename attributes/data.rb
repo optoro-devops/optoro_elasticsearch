2c50edd86f91c654158b26b9208dfce3304dd4b9
Scott Medefind <smedefind@optoro.com>
2015-06-18 16:05:29 -0400

    Removing old data disk, adding aws-cloud plugin

--- attributes/data.rb
+++ attributes/data.rb
@@ -1,17 +1,4 @@
 default['optoro']['ec2']['elasticsearch']['data']['devices'] = {
-  '/dev/xvdf' => {
-    'file_system' => 'ext4',
-    'mount_options' => 'rw,user',
-    'mount_path' => '/elasticsearch/disk1',
-    'format_command' => 'mkfs.ext4',
-    'fs_check_command' => 'dumpe2fs',
-    'ebs' => {
-      'device' => '/dev/sdf',
-      'size' => 1000,
-      'delete_on_termination' => true,
-      'type' => 'gp2'
-    }
-  },
   '/dev/xvdg' => {
     'file_system' => 'ext4',
     'mount_options' => 'rw,user',
