789010b85d151b3ed4d1a063d0328e402f83416d
Scott Medefind <smedefind@optoro.com>
2015-09-28 14:35:01 -0400

    Setting a minimum amount of system RAM on data nodes

--- recipes/data.rb
+++ recipes/data.rb
@@ -5,6 +5,12 @@
 node.set['elasticsearch']['http_enabled'] = 'true'
 node.set['elasticsearch']['node_data'] = 'true'
 node.set['elasticsearch']['node_master'] = 'false'
+
+# Set a minimum amount of system memory to leave free
+if (node['memory']['total'].to_i - (node['elasticsearch']['allocated_memory'].to_i * 1024) / 1024) < 2560
+  node.set['elasticsearch']['allocated_memory'] = (node['memory']['total'].to_i / 1024) - 2560
+end
+
 if node['elasticsearch']['allocated_memory'].to_i > 2048
   node.set['elasticsearch']['env_options'] = '-Xmn256m -XX:SurvivorRatio=3'
 end
